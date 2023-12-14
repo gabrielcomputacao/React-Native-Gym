941328f421fdb60edd3c21de328d3f9d9ad370f4
gabriel <gabrielcomputacao@outlook.com>
2023-12-14 14:45:50 -0300

    feat: create  privates routes

---
+++ gym-app/src/routes/app.routes.tsx
@@ -0,0 +1,88 @@
+import {
+  createBottomTabNavigator,
+  BottomTabNavigationProp,
+} from "@react-navigation/bottom-tabs";
+import { Exercise } from "@screens/Exercise";
+import { History } from "@screens/History";
+import { Home } from "@screens/Home";
+import { Profile } from "@screens/Profile";
+import { Platform } from "react-native";
+
+import HomeSvg from "@assets/home.svg";
+import HistorySvg from "@assets/history.svg";
+import ProfileSvg from "@assets/profile.svg";
+import { useTheme } from "native-base";
+
+type AppRoutesProps = {
+  home: undefined;
+  exercise: undefined;
+  profile: undefined;
+  history: undefined;
+};
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutesProps>;
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutesProps>();
+
+export function AppRoutes() {
+  const { sizes, colors } = useTheme();
+
+  const iconSize = sizes[6];
+
+  return (
+    /* HeaderShown tira o titulo de cima da pagina
+        tabBarLabel tira o nome aparecendo no menu tab se for false
+    */
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: colors.green[500],
+        tabBarInactiveTintColor: colors.gray[200],
+        tabBarStyle: {
+          backgroundColor: colors.gray[600],
+          borderTopWidth: 0,
+          height: Platform.OS === "android" ? "auto" : 96,
+          paddingBottom: sizes[10],
+          paddingTop: sizes[6],
+        },
+      }}
+    >
+      <Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+      <Screen
+        name="history"
+        component={History}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HistorySvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+      <Screen
+        name="profile"
+        component={Profile}
+        options={{
+          /* seta o icone da rota dentro do tabs */
+          tabBarIcon: ({ color }) => (
+            <ProfileSvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+      <Screen
+        name="exercise"
+        component={Exercise}
+        options={{
+          /* deixa nao visualizado o item nos menus tabs */
+          tabBarButton: () => null,
+        }}
+      />
+    </Navigator>
+  );
+}
