5b3dd80c31a5628e7854e6319945a0980afe27d4
gabriel <gabrielcomputacao@outlook.com>
2023-12-14 06:16:32 -0300

    feat: creating Routes

--- gym-app/App.tsx
+++ gym-app/App.tsx
@@ -7,7 +7,7 @@ import {
 import { NativeBaseProvider } from "native-base";
 import { Loading } from "@components/Loading";
 import { THEME } from "./src/theme";
-import { SignIn } from "@screens/Signin";
+import { Routes } from "@routes/index";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
