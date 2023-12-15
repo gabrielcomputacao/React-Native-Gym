f91a8d0155154504c5e9ed39467adb1f6c6165ce
gabriel <gabrielcomputacao@outlook.com>
2023-12-15 14:49:13 -0300

    feat: creating Profile component

--- gym-app/src/screens/History.tsx
+++ gym-app/src/screens/History.tsx
@@ -1,14 +1,50 @@
 import { HistoryCard } from "@components/HistoryCard";
 import { ScreenHeader } from "@components/ScreenHeader";
-import { VStack } from "native-base";
+import { Heading, VStack, SectionList, Text } from "native-base";
+import { useState } from "react";
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: "15.08.23",
+      data: ["costas"],
+    },
+    {
+      title: "16.08.23",
+      data: ["Puxada Frontal"],
+    },
+  ]);
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercícios"></ScreenHeader>
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
+
+      {/* consegue renderizar o componente e consegue dividir por meio de um componente que vai ser um header para formatação de um grupo,
+    o sectionList faz a separação por meio de grupos coisa que o flat list nao faz automatico  */}
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        /* renderiza o titulo e separa pelo que foi colocado no state */
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            {/* quebra de linha no react */}
+            Não há exercícios registrados ainda. {"\n"}
+            Vamos exercitar hoje?
+          </Text>
+        )}
+        contentContainerStyle={
+          exercises.length === 0 && { flex: 1, justifyContent: "center" }
+        }
+        showsVerticalScrollIndicator={false}
+      />
     </VStack>
   );
 }
--- gym-app/src/screens/Profile.tsx
+++ gym-app/src/screens/Profile.tsx
@@ -1,9 +1,69 @@
-import { Center, Text } from "native-base";
+import { Button } from "@components/Button";
+import { Input } from "@components/Input";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import {
+  Center,
+  Heading,
+  ScrollView,
+  Skeleton,
+  Text,
+  VStack,
+} from "native-base";
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(true);
+
   return (
-    <Center flex={1}>
-      <Text>Home</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView>
+        <Center mt={6} px={10}>
+          {photoIsLoading ? (
+            <Skeleton w={PHOTO_SIZE} h={PHOTO_SIZE} rounded="full" />
+          ) : (
+            <UserPhoto
+              source={{ uri: "https://github.com/gabrielcomputacao.png" }}
+              alt="Imagem do usuario"
+              size={PHOTO_SIZE}
+            />
+          )}
+          <TouchableOpacity>
+            <Text
+              color="green.500"
+              fontWeight="bold"
+              fontSize="md"
+              mb={8}
+              mt={2}
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+
+          <Input placeholder="Nome" bg="gray.600" />
+          <Input placeholder="E-mail" bg="gray.600" isDisabled />
+        </Center>
+
+        <VStack px={10} mb={9} mt={12}>
+          <Heading color="gray.200" fontSize="md" mb={2}>
+            Alterar Senha
+          </Heading>
+          <Input bg="gray.600" secureTextEntry placeholder="Senha Antiga" />
+          <Input bg="gray.600" secureTextEntry placeholder="Senha Nova" />
+          <Input
+            bg="gray.600"
+            secureTextEntry
+            placeholder="Confirme a Nova Senha"
+          />
+
+          <Button title="Atualizar" mt={4} />
+        </VStack>
+      </ScrollView>
+    </VStack>
   );
 }
